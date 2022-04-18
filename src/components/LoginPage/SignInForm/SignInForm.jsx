0492620b5ca69b866f233e1532fcb73cc1edbae8
Alex <popowuch1104@gmail.com>
2022-04-19 01:58:56 +0300

    implement sign in functionality

--- src/components/LoginPage/SignInForm/SignInForm.jsx
+++ src/components/LoginPage/SignInForm/SignInForm.jsx
@@ -5,20 +5,50 @@ import { useState } from 'react';
 import { Button, FormControl, IconButton, Input, InputAdornment, InputLabel} from '@mui/material';
 import { Visibility,VisibilityOff} from '@mui/icons-material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { authDataCreator } from '../../../redux/actionCreators/authDataCreator';
+import { useNavigate } from 'react-router-dom';
 
 const SignInForm = ({formsToggle})=>{
   const[isEysClosed,setIsEysClosed] = useState();
+  const[nickname,setNickname] = useState('');
+  const[password,setPassword] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  
+  const onChangeNicknameHandler = (e)=>{
+    setNickname(e.target.value);
+  }
+  const onChangePasswordHandler = (e)=>{
+    setPassword(e.target.value);
+  }
+
+  const onSubmitHandler = (event)=>{
+    event.preventDefault();
+    axios.post('/api/auth/signin',{nickname,password})
+    .then(data=>{
+        dispatch(authDataCreator({isLogIn:true,token:data.token}));
+        navigate('/boards');
+    })
+    .catch(err=>console.log(err.message))
+
+    setNickname('');
+    setPassword('');
+  }
 
   const toggleShowPasswordHandler = () => setIsEysClosed(isEysClosed=>!isEysClosed);
   
   return(
     <div className='sign-in-form-wrapper'>
         <div className='sign-in-form-title'>SignIn</div>
-        <form className='sign-in-form'>
+        <form className='sign-in-form' onSubmit={onSubmitHandler}>
             <FormControl variant="standard">
                 <InputLabel htmlFor="nickname">nickname</InputLabel>
                 <Input
                     id="nickname"
+                    onChange={onChangeNicknameHandler}
+                    value={nickname}
                     endAdornment={
                     <InputAdornment position="end">
                         <IconButton>
@@ -32,6 +62,8 @@ const SignInForm = ({formsToggle})=>{
                 <InputLabel htmlFor="standard-adornment-password">password</InputLabel>
                 <Input
                     id="standard-adornment-password"
+                    onChange={onChangePasswordHandler}
+                    value={password}
                     type={isEysClosed ? 'text' : 'password'}
                     endAdornment={
                     <InputAdornment position="end">
@@ -45,7 +77,7 @@ const SignInForm = ({formsToggle})=>{
                     }
                 />
             </FormControl>
-            <Button className='sign-in-btn' variant="outlined" color="secondary">Sign In</Button>
+            <Button type="submit" className='sign-in-btn' variant="outlined" color="secondary">Sign In</Button>
         </form>
         <div onClick={formsToggle} className="dont-have-account">Don't have an account ?</div>
     </div>

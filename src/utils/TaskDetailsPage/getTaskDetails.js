4c6fa943f33e200cda12447bf04c89cd4773d946
sasha_popovych <sashapopovych2003@gmail.com>
2022-05-18 17:24:12 +0300

    fix bag in task details page

--- src/utils/TaskDetailsPage/getTaskDetails.js
+++ src/utils/TaskDetailsPage/getTaskDetails.js
@@ -4,13 +4,17 @@ import { toast } from 'react-toastify';
 export const getTaskDetails = (taskId,setDetails,navigate) =>{
     const delayForCheckTaskAccess = 2000;
 	const goToPreviousPage = -1;
-    
+    console.log('THIS ISSSS');
     axios
         .get(`/api/task/details/${taskId}`)
         .then(res => {
+            
+            console.log(res);
             setDetails(res.data.details);
         })
         .catch(err => {
+            console.log('Still error!');
+            console.log(err);
             toast.error('Access deny');
             setTimeout(() => {
                 navigate(goToPreviousPage);
